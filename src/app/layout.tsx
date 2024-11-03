e92ba0536de3be2da0021300b6a74cc79dd2f0a6
mohamedx2 <hamroun1969@gmail.com>
2024-11-03 13:39:30 +0100

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
+// src/app/layout.tsx
 import './globals.css';
 
+import { getServerSession } from 'next-auth';
+
 import AppSession from '@/contexts/AppSession';
+import authOptions
+  from '@/lib/authOptions'; // Adjust the import path to where your authOptions are located
 import { NextUIProvider } from '@nextui-org/react';
 
 // Define metadata
 export const metadata = {
-  title: 'My dnd ', // Replace with your title
+  title: 'My dnd', // Replace with your title
   description: 'dnd next js / supabase SQL/next-auth/nextui', // Replace with your description
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body className="antialiased">
-        <AppSession  session={null}>
+        <AppSession session={session}>
           <NextUIProvider>
             {children}
           </NextUIProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,39 @@
 "use client";
-import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+
+import {
+  signOut,
+  useSession,
+} from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+
+import { Button } from '@nextui-org/react';
 
 import Notes from '../components/page';
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   const router = useRouter();
 
-  // Wait until the session has finished loading
-  if (status === 'loading') {
-    return <p>Loading...</p>; // You can replace this with a loading spinner if preferred
-  }
+  useEffect(() => {
+    // Redirect to login if there's no session and status is not loading
+    if (status === 'unauthenticated') {
+      router.push('/login');
+    }
+  }, [status, router]);
 
-  // If session is not available, redirect to login
-  if (!session) {
-    router.push('/login');
-    return null; // Prevents further rendering while redirecting
+  // Show loading message while session is loading
+  if (status === 'loading') {
+    return <p>Loading...</p>;
   }
 
   return (
     <div>
+      <Button onClick={() => signOut()} style={{ marginBottom: '1rem' ,  fontFamily: 'Calibri' }}
+                   
+                       variant="light" className="h-[27px] mt-3 ml-3 w-[97px] rounded-[5px] p-[10px] text-[#D95806] text-[11.5px] font-[400] leading-[20px] text-left border border-[#D95806]">
+        Logout
+      </Button>
       <Notes />
     </div>
   );
