d06db0b9f90de205c1071c0445837b01efbc5f02
mohamedx2 <hamroun1969@gmail.com>
2024-11-03 12:34:36 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <AppSession>
+        <AppSession  session={null}>
           <NextUIProvider>
             {children}
           </NextUIProvider>
