d320dd084be8c9c41821a7d7e87248ff7da64f0b
mohamed ali hamroun <hamroun1969@gmail.com>
2024-12-09 21:26:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// next.config.js
 module.exports = {
-  reactStrictMode: true, // You can keep this as well if needed
-  // Other custom configurations if necessary
-}
+  reactStrictMode: true,
+};
