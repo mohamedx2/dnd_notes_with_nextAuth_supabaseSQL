5d7666406e3089881fb2547fd095b18fd9a8ecec
mohamed ali hamroun <hamroun1969@gmail.com>
2024-12-09 21:21:53 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+  output: 'export', // Enables static export
+  reactStrictMode: true, // You can keep this as well if needed
+  // Other custom configurations if necessary
+}
